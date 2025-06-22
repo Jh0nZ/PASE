d54700df03b66bd0ef3b44a57686cc6eeb41d6d7
Daye0210 <202202138@est.umss.edu>
2025-06-22 01:20:28 -0400

    implementacion para HCE

--- android/app/src/main/java/com/example/pase/model/Navigation.kt
+++ android/app/src/main/java/com/example/pase/model/Navigation.kt
@@ -17,7 +17,7 @@ object NavigationData {
     val items = listOf(
         NavigationItem("Main", Icons.Default.Home, "main"),
         NavigationItem("Validacion", Icons.Default.CheckCircle, "validation"),
-        NavigationItem("Page 3", Icons.Default.Favorite, "page3"),
+        NavigationItem("HCE", Icons.Default.Favorite, "page3"),
         NavigationItem("Page 4", Icons.Default.Person, "page4")
     )
 }
