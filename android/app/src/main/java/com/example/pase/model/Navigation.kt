a14d88e2170202eec58af34c2386f1d86e3613f3
Jh0nZ <jhondeycraft776@gmail.com>
2025-06-22 21:23:00 -0400

    Add NfcView for NFC card processing and enhance navigation with updated icons

--- android/app/src/main/java/com/example/pase/model/Navigation.kt
+++ android/app/src/main/java/com/example/pase/model/Navigation.kt
@@ -1,10 +1,13 @@
 package com.example.pase.model
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.FactCheck
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.FactCheck
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Wallet
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class NavigationItem(
@@ -16,7 +19,7 @@ data class NavigationItem(
 object NavigationData {
     val items = listOf(
         NavigationItem("Main", Icons.Default.Home, "main"),
-        NavigationItem("Validacion", Icons.Default.CheckCircle, "validation"),
+        NavigationItem("Validacion", Icons.AutoMirrored.Filled.FactCheck, "validation"),
         NavigationItem("Page 3", Icons.Default.Favorite, "page3"),
         NavigationItem("Page 4", Icons.Default.Person, "page4")
     )
